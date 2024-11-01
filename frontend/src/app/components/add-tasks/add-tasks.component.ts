f3dee7d2c0758da3b74d9ef453dec532b82a75c7
Kristina Milošević <milosevic.sr14.2022@uns.ac.rs>
2024-11-01 17:59:37 +0100

    Backend done

--- frontend/src/app/components/add-tasks/add-tasks.component.ts
+++ frontend/src/app/components/add-tasks/add-tasks.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @Component({
-  selector: 'app-add-tasks',
+  selector: 'app-create-task',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './add-tasks.component.html',
-  styleUrl: './add-tasks.component.css'
+  styleUrls: ['./add-tasks.component.css']
 })
 export class AddTasksComponent {
 
--- frontend/src/app/services/task/task.service.ts
+++ frontend/src/app/services/task/task.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class TaskService {
 
-  constructor() { }
+export class TaskService {
+  private apiUrl = 'http://localhost:8000/tasks';
+
+  constructor(private http: HttpClient) {}
+
+  // Metoda za slanje POST zahteva za kreiranje novog taska
+  createTask(taskData: { projectId: string; title: string; description: string }): Observable<any> {
+    const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
+    return this.http.post(this.apiUrl, taskData, { headers });
+  }
 }
