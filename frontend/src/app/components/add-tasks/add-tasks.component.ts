e986bc27c6bc6b8b86bfe91be03a536c71139b7f
Kristina Milošević <milosevic.sr14.2022@uns.ac.rs>
2024-11-01 13:32:52 +0100

    Adding components

---
+++ frontend/src/app/components/add-tasks/add-tasks.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-tasks',
+  standalone: true,
+  imports: [],
+  templateUrl: './add-tasks.component.html',
+  styleUrl: './add-tasks.component.css'
+})
+export class AddTasksComponent {
+
+}
